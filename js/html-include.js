990f3510c9ade7491244f9361d32d9f452837f40
Tristan Guichaoua <baanlohdev@gmail.com>
2020-12-05 14:28:42 +0100

    base files

---
+++ js/html-include.js
@@ -0,0 +1,16 @@
+class HTMLInclude extends HTMLElement {
+    constructor(src) {
+        super();
+        this.attachShadow({ mode: "open" });
+        if (src) this.textContent = src;
+        setTimeout(() => this._load());
+    }
+    async _load() {
+        const src = this.textContent.trim();
+        if (!src) throw new Error("URL missing between <html-import> tags.");
+        const rsp = await fetch(src);
+        if (rsp.status != 200) throw new Error(`Failed to load file (${src}) for <html-import>.`);
+        this.shadowRoot.innerHTML = await rsp.text();
+    }
+}
+customElements.define("html-include", HTMLInclude);
---
+++ js/page-view.js
@@ -0,0 +1,15 @@
+class PageView extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        setTimeout(() => this._load());
+    }
+    async _load() {
+        const page = window.location.search.substring(1);
+        const src = `./pages/${page === "" ? "accueil" : page}.html`;
+        const rsp = await fetch(src);
+        if (rsp.status != 200) window.location.href = "/";
+        else this.shadowRoot.innerHTML = await rsp.text();
+    }
+}
+customElements.define("page-view", PageView);
