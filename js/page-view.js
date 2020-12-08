25c0e998c8ca5b2afc435a4eadfe032cf6441c03
Tristan Guichaoua <33934311+tguichaoua@users.noreply.github.com>
2020-12-08 10:29:30 +0100

    fix page-view (#4)

--- js/page-view.js
+++ js/page-view.js
@@ -1,15 +1,14 @@
-class HTMLInclude extends HTMLElement {
-    constructor(src) {
+class PageView extends HTMLElement {
+    constructor() {
         super();
-        if (src) this.textContent = src;
         setTimeout(() => this._load());
     }
     async _load() {
-        const src = this.textContent.trim();
-        if (!src) throw new Error("URL missing between <html-include> tags.");
+        const page = window.location.search.substring(1);
+        const src = `./pages/${page === "" ? "accueil" : page}.html`;
         const rsp = await fetch(src);
-        if (rsp.status !== 200) throw new Error(`Failed to load file (${src}) for <html-include>.`);
+        if (rsp.status !== 200) window.location.href = "/";
         this.innerHTML = await rsp.text();
     }
 }
-customElements.define("html-include", HTMLInclude);
+customElements.define("page-view", PageView);
