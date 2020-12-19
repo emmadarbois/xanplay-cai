133bb5f3c446da61c62b3e0c7e88329527b4fa93
LE GALL Marine <m7legall@enib.fr>
2020-12-19 17:36:58 +0100

    ajout jeux

---
+++ js/filtres.js
@@ -0,0 +1,13 @@
+
+function btn_filter_action(e) {
+	console.log("je passe dans la fonction");
+	let j1=document.getElementById('jeu_bmc');
+	let j2=document.getElementById('jeu_bmc');
+	let j3=document.getElementById('jeu_bmc');	
+	let j4=document.getElementById('jeu_bmc');
+	let j5=document.getElementById('jeu_bmc');
+	let j6=document.getElementById('jeu_bmc');
+	let j7=document.getElementById('jeu_bmc');
+
+	j1.remove();
+}
