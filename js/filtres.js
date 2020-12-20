af9d3df3474fca083482c8055beb329e79791bca
LE GALL Marine <m7legall@enib.fr>
2020-12-20 18:37:38 +0100

    ajout action filtre + organisation

--- js/filtres.js
+++ js/filtres.js
@@ -1,13 +1,25 @@
 
 function btn_filter_action(e) {
-	console.log("je passe dans la fonction");
 	let j1=document.getElementById('jeu_bmc');
-	let j2=document.getElementById('jeu_bmc');
-	let j3=document.getElementById('jeu_bmc');	
-	let j4=document.getElementById('jeu_bmc');
-	let j5=document.getElementById('jeu_bmc');
-	let j6=document.getElementById('jeu_bmc');
-	let j7=document.getElementById('jeu_bmc');
+	let j2=document.getElementById('jeu_pp');
+	let j3=document.getElementById('jeu_mafia');	
+	let j4=document.getElementById('jeu_bang');
+	let j5=document.getElementById('jeu_magic');
 
-	j1.remove();
+	
+	if(getComputedStyle(j1).display != "none"){
+    j1.style.display="none";
+    j2.style.display="none";
+    j3.style.display="none";
+    j4.style.display="none";
+    j5.style.display="none";
+    
+  } else {
+    j1.style.display = "block";
+ 	j2.style.display = "block";
+  	j3.style.display = "block";
+   	j4.style.display = "block";
+  	j5.style.display = "block";
+  
+  }
 }
