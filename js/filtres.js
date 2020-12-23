3d5a4439de2fdd7d71a582596cf8371dc89871e7
LE GALL Marine <m7legall@enib.fr>
2020-12-23 16:38:24 +0100

    commentaire

--- js/filtres.js
+++ js/filtres.js
@@ -5,7 +5,7 @@ function btn_filter_action(e) {
 	let j3=document.getElementById('jeu_mafia');	
 	let j4=document.getElementById('jeu_bang');
 	let j5=document.getElementById('jeu_magic');
-
+//on recupère tout les jeux qui n'ont pas le tag "action"
 	
 	if(getComputedStyle(j1).display != "none"){
     j1.style.display="none";
@@ -13,13 +13,13 @@ function btn_filter_action(e) {
     j3.style.display="none";
     j4.style.display="none";
     j5.style.display="none";
-    
+    //quand on coche on les efface
   } else {
     j1.style.display = "block";
  	j2.style.display = "block";
   	j3.style.display = "block";
    	j4.style.display = "block";
   	j5.style.display = "block";
-  
+    //quand on décoche on les remets 
   }
 }
